f5f0a2e8dbe65ec81a8e29b81c4703a2f760810b
Greg1928 <gregoriosortino@protonmail.com>
2022-07-12 17:45:57 +0200

    current date departure trains

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Train;
+use Carbon\Carbon;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+        $mytime = Carbon::now()->format('Y-m-d');
+        $trains = Train::where('data_partenza',$mytime)->get();
+        dd($trains);
+    }
+}
